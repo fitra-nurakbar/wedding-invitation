c3bdc3a6fb9332a7554cc40dc08861245bd8c4cd
fitra-nurakbar <fitranurakbar378@gmail.com>
2026-01-14 14:34:34 +0700

    add icons menu

--- components/Menu.tsx
+++ components/Menu.tsx
@@ -1,31 +1,34 @@
 'use client';
 
 import Link from "next/link";
+import { IconProps } from "@/utils/types/icons";
+import { CoupleIcon, CoverIcon, EventIcon, GalleryIcon, WhishesIcon } from "@/components/IconsMenu";
 
 export default function Menu() {
   return (
     <div className="sticky bottom-0 z-20">
       <nav className="bg-linear-to-b from-[#be6462] to-[#913736] rounded-t-lg w-full">
-        <div className="linear grid grid-cols-5 divide-x-2 text-center text-xs font-medium">
-          <MenuItem label="cover" target="cover" />
-          <MenuItem label="couple" target="couple" />
-          <MenuItem label="event" target="event" />
-          <MenuItem label="gallery" target="gallery" />
-          <MenuItem label="wishes" target="wishes" />
+        <div className="linear grid grid-cols-5 divide-x-2 text-center text-xs">
+          <MenuItem label="cover" target="cover" icon={CoverIcon} />
+          <MenuItem label="couple" target="couple" icon={CoupleIcon} />
+          <MenuItem label="event" target="event" icon={EventIcon} />
+          <MenuItem label="gallery" target="gallery" icon={GalleryIcon} />
+          <MenuItem label="wishes" target="wishes" icon={WhishesIcon} />
         </div>
       </nav>
     </div>
   );
 }
 
-function MenuItem({ label, target }: { label: string; target: string }) {
+function MenuItem({ label, target, icon: Icon, }: { label: string; target: string; icon: React.FC<IconProps>; }) {
 
 
   return (
-    <Link href={`#${label}`} className="border-white">
-      <button  className="text-white cursor-pointer hover:text-primary transition-colors py-3 capitalize">
-          {label}
-        </button>
+    <Link href={`#${target}`} className="border-white">
+      <button className="text-white cursor-pointer hover:text-primary transition-colors pt-1 pb-2 capitalize">
+        <Icon className="w-10 h-10 fill-white" />
+        <span>{label}</span>
+      </button>
     </Link>
   );
 }
