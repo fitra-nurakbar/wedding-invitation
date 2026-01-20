61f013076d77bcd52a075eea88a21678428d57d4
fitra-nurakbar <fitranurakbar378@gmail.com>
2026-01-20 16:29:26 +0700

    add event part

--- utils/eventDetails.tsx
+++ utils/eventDetails.tsx
@@ -7,7 +7,7 @@ export const eventDetails: EventDetail[] = [
     time: "08:00 - 10:00",
     place: "Kediaman Mempelai Wanita",
     address: "Lingk. Lembur Balong RT.01/RW.08 - Kota Banjar",
-    mapUrl: "https://maps.google.com/?q=Masjid+Agung+Samarinda",
+    mapUrl: "https://goo.gl/maps/wRyvokGq3bsBueJE9?g_st=awb",
   },
   {
     title: "Resepsi",
@@ -15,14 +15,14 @@ export const eventDetails: EventDetail[] = [
     time: "10:00 - Selesai",
     place: "Kediaman Mempelai Wanita",
     address: "Lingk. Lembur Balong RT.01/RW.08 - Kota Banjar",
-    mapUrl: "https://maps.google.com/?q=Gedung+Serbaguna+Samarinda",
+    mapUrl: "https://goo.gl/maps/wRyvokGq3bsBueJE9?g_st=awb",
   },
   {
     title: "Ngunduh Mantu",
-    date: "Sabtu, 04 Maret 20226",
+    date: "Sabtu, 04 April 2026",
     time: "08:00 - Selesai",
     place: "Kediaman Mempelai Pria",
     address: "JL. Pasir Impun, Komplek Taman Melati, Blok C2 NO. 13 - Kab. Bandung",
-    mapUrl: "https://maps.google.com/?q=Gedung+Serbaguna+Samarinda",
+    mapUrl: "https://goo.gl/maps/eq86nYDYvC457E639?g_st=aw",
   },
 ]
