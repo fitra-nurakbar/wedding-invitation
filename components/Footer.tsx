e82f9beee014d95b79282f9b6d47606bbaf29b27
fitra-nurakbar <fitranurakbar378@gmail.com>
2026-01-23 20:14:04 +0700

    add gallery images

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -9,8 +9,8 @@ export default function Footer() {
         <footer className="bg-background min-h-64 mb-0 text-xs text-white flex flex-col gap-2 text-center items-center justify-center">
             <LogoIcon className='size-15' />
             <Link href={"https://instagram.com/fit.tra_"} target='_blank'>
-            <p>© 2026 Made with Love by</p>
-            <p>Fitra Nurakbar</p>
+            <p>© 2026 Build with</p>
+            <p>@fit.tra_</p>
         </Link>
         </footer >
     )
