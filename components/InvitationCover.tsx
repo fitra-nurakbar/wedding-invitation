9b500328d82d39bbf6527ca62a9ca62c81364089
fitra-nurakbar <fitranurakbar378@gmail.com>
2026-01-24 08:52:20 +0700

    add curtain

--- components/InvitationCover.tsx
+++ components/InvitationCover.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import { useAnimation } from './animations/AnimationProvider'
 import FadeDown from './animations/FadeDown'
 import FadeLeft from './animations/FadeLeft'
@@ -13,8 +13,9 @@ import ScaleUp from './animations/ScaleUp'
 export default function InvitationCover({ guestName }: { guestName: string }) {
   const { opened, openInvitation } = useMusic()
   const { openAnimation } = useAnimation()
+  const [showCurtain, setShowCurtain] = useState(false)
+  const [isClosing, setIsClosing] = useState(false)
 
-  // lock scroll
   useEffect(() => {
     document.body.style.overflow = opened ? '' : 'hidden'
     return () => {
@@ -23,78 +24,107 @@ export default function InvitationCover({ guestName }: { guestName: string }) {
   }, [opened])
 
   const handleOpen = async () => {
+    setIsClosing(true)
     openAnimation()
+
+    await new Promise(res => setTimeout(res, 1200))
+    setShowCurtain(true)
+
+    await new Promise(res => setTimeout(res, 1200))
     await openInvitation()
   }
 
   if (opened) return null
 
   return (
-    <div className="
+    <div className={`
       fixed inset-0 z-50
       flex items-center justify-center
       h-dvh
-      bg-linear-to-b from-gray-100 via-white to-gray-400
-    ">
-      <div className="
-        flex flex-col justify-center items-center
-        text-center
-        gap-[2vh]
-        px-[6vw]
-        min-h-[85dvh]
-      ">
-        {/* Initial */}
-        <h1 className="flex items-center justify-center font-cinzel text-[12vh] leading-none">
-          <FadeLeft>I</FadeLeft>
-          <FadeRight>J</FadeRight>
-        </h1>
-        <ScaleUp>
-          <SeparatorIcon className='max-w-[35vh]' />
-        </ScaleUp>
-        <FadeDown>
-          <p className="text-[1.5vh] font-roboto tracking-widest text-gray-400">
-            The Wedding Of
+      overflow-hidden
+    `}>
+
+      {/* ===== CONTENT ===== */}
+      <div
+        className={`
+          relative w-full flex justify-center items-center z-10
+          ${isClosing ? 'animate-fade-out-up' : ''}
+        `}
+      >
+        <div className="
+          absolute rounded-t-full rounded-b-xl border-double border-4
+          w-full max-w-xs h-full mx-auto my-auto
+          max-h-[calc(100vh-10rem)]
+          flex flex-col justify-center items-center
+          text-center gap-[2vh] min-h-[85dvh]
+        ">
+          <h1 className="flex items-center justify-center font-cinzel text-[12vh] leading-none">
+            <FadeLeft>I</FadeLeft>
+            <FadeRight>J</FadeRight>
+          </h1>
+
+          <ScaleUp>
+            <SeparatorIcon className="max-w-[35vh]" />
+          </ScaleUp>
+
+          <FadeDown>
+            <p className="text-[1.5vh] font-roboto tracking-widest text-gray-400">
+              The Wedding Of
+            </p>
+          </FadeDown>
+
+          <FadeUp>
+            <h2 className="text-[3.5vh] font-ivy-mode mb-[4vh]">
+              Ifha & Jefri
+            </h2>
+          </FadeUp>
+
+          <p className="text-[1.6vh] leading-relaxed">
+            Kepada Yth. <br />
+            Bapak / Ibu / Saudara / i
           </p>
-        </FadeDown>
 
-        <FadeUp>
-          <h2 className="text-[3.5vh] font-ivy-mode mb-[4vh]">
-            Ifha & Jefri
-          </h2>
-        </FadeUp>
+          <FadeDown>
+            <h3 className="text-[2.5vh] font-bold px-[5vw]">
+              {guestName}
+            </h3>
+          </FadeDown>
 
-        <p className="text-[1.6vh] leading-relaxed">
-          Kepada Yth. <br />
-          Bapak / Ibu / Saudara / i
-        </p>
-
-        <FadeDown>
-          <h3 className="text-[2.5vh] font-bold px-[5vw]">
-            {guestName}
-          </h3>
-        </FadeDown>
-
-        <FadeUp>
-          <button
-            onClick={handleOpen}
-            className="
-              mt-[4vh]
-              px-6 py-2
-              bg-white
-              text-gray-500
-              text-sm
-              active:scale-110
-              transition
-              cursor-pointer
-            "
-          >
-            <div className="flex gap-2 items-center">
-              <Envelope className="size-5" />
-              <span>Buka Undangan</span>
-            </div>
-          </button>
-        </FadeUp>
+          <FadeUp>
+            <button
+              onClick={handleOpen}
+              className="
+                px-6 py-2 mt-4
+                bg-white
+                text-gray-500
+                text-sm
+                active:scale-110
+                transition
+                duration-300
+                cursor-pointer
+              "
+            >
+              <div className="flex gap-2 items-center">
+                <Envelope className="size-5" />
+                <span>Buka Undangan</span>
+              </div>
+            </button>
+          </FadeUp>
+        </div>
       </div>
+
+      <div className={`
+            fixed inset-y-0 left-0 z-5
+            w-1/2 bg-white
+            origin-left
+            ${showCurtain && ('animate-curtain-left')}
+          `} />
+      <div className={`
+            fixed inset-y-0 right-0 z-5
+            w-1/2 bg-white
+            origin-right
+            ${showCurtain && ('animate-curtain-right')}
+          `} />
     </div>
   )
 }
--- components/cover/Cover.tsx
+++ components/cover/Cover.tsx
@@ -23,7 +23,7 @@ export default function Cover() {
   return (
     <section id="cover" className="flex flex-col items-center justify-center">
       <div className="text-center py-10 min-h-screen">
-        <FadeDown requireMusic once className="w-full flex justify-center my-5">
+        <FadeDown requireMusic once delay={3} className="w-full flex justify-center my-5">
           <div className="relative w-full max-w-50 max-h-75 aspect-2/4 rounded-full overflow-hidden border-7 border-double border-primary">
             <Image
               src={profile.src}
@@ -35,7 +35,7 @@ export default function Cover() {
             />
           </div>
         </FadeDown>
-        <FadeUp requireMusic once delay={1} >
+        <FadeUp requireMusic once delay={4} >
           <p className="text-sm font-roboto font-light tracking-widest mb-3">
             The Wedding Of
           </p>
@@ -50,9 +50,9 @@ export default function Cover() {
           <Countdown />
         </FadeUp>
       </div>
-      <div className="relative h-120">
+      <div className="relative h-100">
         <BackgroundImage images={images} gradient="rounded" />
-        <div className="relative flex flex-col items-center justify-center gap-10 text-center px-5 py-10 h-full">
+        <div className="relative flex flex-col items-center justify-end gap-10 text-center px-5 py-10 h-full">
           <div className="text-white flex justify-center items-center">
             <FadeLeft requireMusic>
               <h3 className='text-7xl font-cinzel mb-5'>I</h3>
@@ -62,15 +62,15 @@ export default function Cover() {
             </FadeRight>
           </div>
           <FadeUp requireMusic>
-            <p className="text-sm text-white">
-              <q>Dan di antara tanda-tanda (kebesaran)-Nya ialah Dia menciptakan pasangan-pasangan untukmu dari jenismu sendiri, agar kamu cenderung dan merasa tenteram kepadanya, dan Dia menjadikan di antaramu rasa kasih dan sayang.</q>
+            <p className="text-sm/tight font-normal text-white">
+              Dan di antara tanda-tanda (kebesaran)-Nya ialah Dia menciptakan pasangan-pasangan untukmu dari jenismu sendiri, agar kamu cenderung dan merasa tenteram kepadanya, dan Dia menjadikan di antaramu rasa kasih dan sayang.
               <br />
               <br />
               (QS. Ar-Rum: 21)
             </p>
           </FadeUp>
         </div>
-        <Tent className="absolute w-full inset-0 z-40 -top-1 m-0 p-0 text-gray-100" />
+        <Tent className="absolute w-full inset-0 z-10 -top-1 m-0 p-0 text-gray-100" />
       </div>
     </section>
   );
